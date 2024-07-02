b40fd4582d0779d6bb0a89b6b4f5602490a079e4
Rares <iraresdeac@gmail.com>
2024-07-02 18:56:06 +0200

    Portfolio

---
+++ enviar_email.php
@@ -0,0 +1,19 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = $_POST['name'];
+    $email = $_POST['email'];
+    $mensaje = $_POST['message'];
+    
+    $destinatario = "contact@raresdproductions.com"; // Cambia esto por tu dirección de correo electrónico
+    $asunto = "Nuevo mensaje de contacto";
+
+    // Cuerpo del correo
+    $contenido = "Nombre: $nombre\n";
+    $contenido .= "Email: $email\n";
+    $contenido .= "Mensaje:\n$mensaje";
+
+    // Envío del correo
+    mail($destinatario, $asunto, $contenido);
+
+}
+?>
